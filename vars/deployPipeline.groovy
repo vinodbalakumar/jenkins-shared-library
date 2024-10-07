00b2e8a156b632245a922f5268d2719d465943ed
DESKTOP-G9OV8MO\HP <vinodbalakumar.p@gmail.com>
2024-10-07 15:48:44 -0400

    test

--- vars/deployPipeline.groovy
+++
@@ -1,4 +0,0 @@
-def call(String dockerRepo, String kubeconfigId, String awsCredentialsId) {
-    def deployPipeline = new org.vinod.DeployPipeline()
-    deployPipeline.runPipeline(this, dockerRepo, kubeconfigId, awsCredentialsId)
-}
